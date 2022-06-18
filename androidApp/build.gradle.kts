2a14813c3302acd805002ad8afe140cb1f4aa999
Lee <rkdmf1026@naver.com>
2022-06-18 18:31:35 +0900

    [ADD] Libarary
    Add Jetpack components, network, image library

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,11 +17,49 @@ android {
             isMinifyEnabled = false
         }
     }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
 }
 
 dependencies {
+    val lifecycle_version = "2.5.0-rc01"
+    val retrofit_version = "2.9.0"
+    val okhttp_version = "4.9.1"
+    val glide_version = "4.13.0"
+
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+
+    //kakao login
+    implementation("com.kakao.sdk:v2-user:2.10.0")
+
+    //viewModel & livedata
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+
+    //ktx
+    implementation ("androidx.activity:activity-ktx:1.4.0")
+
+    //Retrofit2
+    implementation ("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation ("com.squareup.retrofit2:converter-gson:$retrofit_version")
+
+    //okhttp
+    implementation ("com.squareup.okhttp3:okhttp:$okhttp_version")
+    implementation ("com.squareup.okhttp3:logging-interceptor:$okhttp_version")
+    implementation ("com.squareup.okhttp3:okhttp-urlconnection:$okhttp_version")
+
+    //Coroutine
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+
+    //gson
+    implementation ("com.google.code.gson:gson:2.8.9")
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:$glide_version")
+    annotationProcessor ("com.github.bumptech.glide:compiler:$glide_version")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri( "https://devrepo.kakao.com/nexus/content/groups/public/") }
     }
 }
 
