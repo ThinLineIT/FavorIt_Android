217cadb41bcdd6940f80dc43032518693e700247
Lee <rkdmf1026@naver.com>
2022-06-18 18:53:00 +0900

    [REFACTOR] change gradle value name

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -24,10 +24,10 @@ android {
 }
 
 dependencies {
-    val lifecycle_version = "2.5.0-rc01"
-    val retrofit_version = "2.9.0"
-    val okhttp_version = "4.9.1"
-    val glide_version = "4.13.0"
+    val LIFECYCLE_VERSION = "2.5.0-rc01"
+    val RETROFIT_VERSION = "2.9.0"
+    val OKHTTP_VERSION = "4.9.1"
+    val GLIDE_VERSIOON = "4.13.0"
 
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.4.0")
@@ -38,28 +38,28 @@ dependencies {
     implementation("com.kakao.sdk:v2-user:2.10.0")
 
     //viewModel & livedata
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$LIFECYCLE_VERSION")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$LIFECYCLE_VERSION")
 
     //ktx
-    implementation ("androidx.activity:activity-ktx:1.4.0")
+    implementation("androidx.activity:activity-ktx:1.4.0")
 
     //Retrofit2
-    implementation ("com.squareup.retrofit2:retrofit:$retrofit_version")
-    implementation ("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    implementation("com.squareup.retrofit2:retrofit:$RETROFIT_VERSION")
+    implementation("com.squareup.retrofit2:converter-gson:$RETROFIT_VERSION")
 
     //okhttp
-    implementation ("com.squareup.okhttp3:okhttp:$okhttp_version")
-    implementation ("com.squareup.okhttp3:logging-interceptor:$okhttp_version")
-    implementation ("com.squareup.okhttp3:okhttp-urlconnection:$okhttp_version")
+    implementation("com.squareup.okhttp3:okhttp:$OKHTTP_VERSION")
+    implementation("com.squareup.okhttp3:logging-interceptor:$OKHTTP_VERSION")
+    implementation("com.squareup.okhttp3:okhttp-urlconnection:$OKHTTP_VERSION")
 
     //Coroutine
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
 
     //gson
-    implementation ("com.google.code.gson:gson:2.8.9")
+    implementation("com.google.code.gson:gson:2.8.9")
 
     //Glide
-    implementation ("com.github.bumptech.glide:glide:$glide_version")
-    annotationProcessor ("com.github.bumptech.glide:compiler:$glide_version")
+    implementation("com.github.bumptech.glide:glide:$GLIDE_VERSIOON")
+    annotationProcessor("com.github.bumptech.glide:compiler:$GLIDE_VERSIOON")
 }
