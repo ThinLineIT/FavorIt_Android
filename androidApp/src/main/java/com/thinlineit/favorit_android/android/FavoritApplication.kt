6671c8a3783092e563b826ce256fdfbdb248f375
Lee <rkdmf1026@naver.com>
2022-06-19 05:11:44 +0900

    [ADD] KakaoLogin setting

--- androidApp/src/main/java/com/thinlineit/favorit_android/android/FavoritApplication.kt
+++ androidApp/src/main/java/com/thinlineit/favorit_android/android/FavoritApplication.kt
@@ -2,11 +2,14 @@ package com.thinlineit.favorit_android.android
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
-import com.thinlineit.favorit_android.android.util.FavoritSharedPreference
+import com.thinlineit.favorit_android.android.data.local.FavoritSharedPreference
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class FavoritApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        KakaoSdk.init(this, BuildConfig.KAKAO_API_KEY)
         FavoritSharedPreference.init(applicationContext)
     }
 }
