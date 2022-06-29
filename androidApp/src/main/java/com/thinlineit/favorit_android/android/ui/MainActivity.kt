b0abab64fabe37b81ca373533ce7c31490ff8df3
yunkwon-shin <shin.yunkwon@linecorp.com>
2022-06-29 21:37:40 +0900

    Clean code on MainActivity and LoginActivity

--- androidApp/src/main/java/com/thinlineit/favorit_android/android/ui/MainActivity.kt
+++ androidApp/src/main/java/com/thinlineit/favorit_android/android/ui/MainActivity.kt
@@ -1,16 +1,9 @@
 package com.thinlineit.favorit_android.android.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.kakao.sdk.common.util.Utility
-import com.thinlineit.favorit_android.Greeting
 import com.thinlineit.favorit_android.android.databinding.ActivityMainBinding
 import com.thinlineit.favorit_android.android.ui.login.LoginActivity
-
-fun greet(): String {
-    return Greeting().greeting()
-}
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,20 +13,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initButtonClickListener()
-        getHashKey()
     }
 
     private fun initButtonClickListener() {
         binding.apply {
             loginButton.setOnClickListener {
-                LoginActivity.startLoginActivity(this@MainActivity)
+                LoginActivity.start(this@MainActivity)
+            }
             }
         }
-    }
-
-    //TODO : 4명(윤권,민기,정민,동기) keyHash를 developer에 등록하면 삭제
-    private fun getHashKey() {
-        var keyHash = Utility.getKeyHash(this)
-        Log.d("kakaoHash", keyHash)
-    }
-}
+    }}
