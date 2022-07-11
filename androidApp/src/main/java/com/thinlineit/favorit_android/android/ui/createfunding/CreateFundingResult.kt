5952c8723482c78a072b6c2f0d1ec40d46d076a5
yunkwon-shin <shin.yunkwon@linecorp.com>
2022-07-11 23:16:40 +0900

    Change SerailizedName of fundingLink in CreateFundingResult

--- androidApp/src/main/java/com/thinlineit/favorit_android/android/ui/createfunding/CreateFundingResult.kt
+++ androidApp/src/main/java/com/thinlineit/favorit_android/android/ui/createfunding/CreateFundingResult.kt
@@ -3,7 +3,7 @@ package com.thinlineit.favorit_android.android.ui.createfunding
 import com.google.gson.annotations.SerializedName
 
 data class CreateFundingResult(
-    @SerializedName("product_link")
+    @SerializedName("link_for_sharing")
     val fundingLink: String,
     @SerializedName("funding_id")
     val fundingID: Int,
