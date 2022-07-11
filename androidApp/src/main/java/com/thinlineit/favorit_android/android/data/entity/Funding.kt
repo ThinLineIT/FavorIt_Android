ceebc85bac7410ba3ced67eb48aaab814db087dd
yunkwon-shin <shin.yunkwon@linecorp.com>
2022-07-11 23:19:28 +0900

    Add FundingDetailActivity

---
+++ androidApp/src/main/java/com/thinlineit/favorit_android/android/data/entity/Funding.kt
@@ -0,0 +1,29 @@
+package com.thinlineit.favorit_android.android.data.entity
+
+import com.google.gson.annotations.SerializedName
+
+data class Funding(
+    val name: String,
+    @SerializedName("contents")
+    val description: String,
+    val state: FundingState,
+    @SerializedName("is_maker")
+    val isMaker: Boolean,
+    @SerializedName("due_date")
+    val expiredDate: String,
+    @SerializedName("progress_percent")
+    val progressPercentage: Int,
+    @SerializedName("link_for_sharing")
+    val fundingLink: String,
+    val product: Product,
+)
+
+enum class FundingState {
+    OPENED, EXPIRED, CLOSED
+}
+
+data class Product(
+    val link: String,
+    val option: String,
+    val price: Int,
+)
