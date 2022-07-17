ca563aa59e3b4387dca2d9480e868658bf05fcfa
Hong min ki <gaogai2@naver.com>
2022-07-18 02:00:59 +0900

    Add getAccessToken to MainActivity.kt

--- androidApp/src/main/java/com/thinlineit/favorit_android/android/ui/MainActivity.kt
+++ androidApp/src/main/java/com/thinlineit/favorit_android/android/ui/MainActivity.kt
@@ -2,13 +2,20 @@ package com.thinlineit.favorit_android.android.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.thinlineit.favorit_android.android.R
+import com.thinlineit.favorit_android.android.data.local.LocalPreferenceDataSource
 import com.thinlineit.favorit_android.android.databinding.ActivityMainBinding
 import com.thinlineit.favorit_android.android.ui.createfunding.CreateFundingActivity
 import com.thinlineit.favorit_android.android.ui.detail.FundingDetailActivity
 import com.thinlineit.favorit_android.android.ui.login.LoginActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    @Inject
+    lateinit var localPreferenceDataSource: LocalPreferenceDataSource
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,5 +36,14 @@ class MainActivity : AppCompatActivity() {
                 FundingDetailActivity.start(this@MainActivity, 3)
             }
         }
+    }
+    override fun onResume() {
+        if (localPreferenceDataSource.getAccessToken() != null
+        ) {
+            binding.loginButton.setImageResource(R.drawable.icon_login_success)
+        } else {
+            binding.loginButton.setImageResource(R.drawable.icon_login)
+        }
+        super.onResume()
     }
 }
