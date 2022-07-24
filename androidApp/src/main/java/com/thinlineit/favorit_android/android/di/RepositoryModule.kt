50300d08bfdc9a43eccd55ff55f15d38aa0d2f35
Lee <rkdmf1026@naver.com>
2022-07-24 20:18:00 +0900

    Remove wildcard import

--- androidApp/src/main/java/com/thinlineit/favorit_android/android/di/RepositoryModule.kt
+++ androidApp/src/main/java/com/thinlineit/favorit_android/android/di/RepositoryModule.kt
@@ -3,7 +3,12 @@ package com.thinlineit.favorit_android.android.di
 import com.thinlineit.favorit_android.android.data.api.FundingApi
 import com.thinlineit.favorit_android.android.data.local.LocalPreferenceDataSource
 import com.thinlineit.favorit_android.android.data.remote.AuthDataSource
-import com.thinlineit.favorit_android.android.data.repository.*
+import com.thinlineit.favorit_android.android.data.repository.AuthRepository
+import com.thinlineit.favorit_android.android.data.repository.AuthRepositoryImpl
+import com.thinlineit.favorit_android.android.data.repository.FundingRepository
+import com.thinlineit.favorit_android.android.data.repository.FundingRepositoryImpl
+import com.thinlineit.favorit_android.android.data.repository.PresentRepository
+import com.thinlineit.favorit_android.android.data.repository.PresentRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
