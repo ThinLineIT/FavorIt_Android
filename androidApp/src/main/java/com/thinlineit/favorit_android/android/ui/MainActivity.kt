61452f7bd7fd016cc8ee710338a65882db2920ae
Lee <rkdmf1026@naver.com>
2022-09-13 03:23:21 +0900

    Refactor MainActivity
    랜딩페이지 레이아웃 적용

--- androidApp/src/main/java/com/thinlineit/favorit_android/android/ui/MainActivity.kt
+++ androidApp/src/main/java/com/thinlineit/favorit_android/android/ui/MainActivity.kt
@@ -3,15 +3,12 @@ package com.thinlineit.favorit_android.android.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.thinlineit.favorit_android.android.R
 import com.thinlineit.favorit_android.android.databinding.ActivityMainBinding
 import com.thinlineit.favorit_android.android.ui.createfunding.CreateFundingActivity
 import com.thinlineit.favorit_android.android.ui.detail.FundingDetailActivity
 import com.thinlineit.favorit_android.android.ui.landing.MainViewModel
-import com.thinlineit.favorit_android.android.ui.login.LoginActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -29,24 +26,11 @@ class MainActivity : AppCompatActivity() {
             lifecycleOwner = this@MainActivity
             this.mainViewModel = viewModel
         }
-
-        viewModel.isLoggedIn.observe(this) {
-            if (it) {
-                binding.loginButton.setImageResource(R.drawable.icon_login_profile)
-            } else {
-                binding.loginButton.setImageResource(R.drawable.icon_login)
-            }
-        }
-
-        initObserver()
         initButtonClickListener()
     }
 
     private fun initButtonClickListener() {
         binding.apply {
-            loginButton.setOnClickListener {
-                LoginActivity.start(this@MainActivity)
-            }
             createFundingButton.setOnClickListener {
                 CreateFundingActivity.start(this@MainActivity)
             }
@@ -54,27 +38,6 @@ class MainActivity : AppCompatActivity() {
                 FundingDetailActivity.start(this@MainActivity, 3)
             }
         }
-    }
-
-    private fun initObserver() {
-        viewModel.isLoggedIn.observe(this) {
-            logInButtonVisibility(it)
-        }
-    }
-
-    private fun logInButtonVisibility(isLoggedIn: Boolean) {
-        if (isLoggedIn) {
-            binding.loginButton.visibility = View.GONE
-            binding.profileButton.visibility = View.VISIBLE
-        } else {
-            binding.loginButton.visibility = View.VISIBLE
-            binding.profileButton.visibility = View.GONE
-        }
-    }
-
-    override fun onResume() {
-        viewModel.checkIsLoggedIn()
-        super.onResume()
     }
 
     companion object {
