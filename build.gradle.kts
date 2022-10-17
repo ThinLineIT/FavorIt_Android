c7baea094d6c66569708f0bedca129f75607ec3d
Lee <rkdmf1026@naver.com>
2022-10-17 21:37:16 +0900

    Set jetpack compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 buildscript {
+    val compose_version by extra("1.2.0")
     repositories {
         gradlePluginPortal()
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
         classpath("com.android.tools.build:gradle:7.2.1")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.0")
