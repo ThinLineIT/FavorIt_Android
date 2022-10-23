468448ce85d7408fd1a484146aaecee0d7be6d6e
rkdmf1026 <rkdmf1026@naver.com>
2022-10-23 14:50:00 +0900

    Refactor FundingListActivity

--- androidApp/src/main/java/com/thinlineit/favorit_android/android/ui/fundingList/FundingListActivity.kt
+++ androidApp/src/main/java/com/thinlineit/favorit_android/android/ui/fundingList/FundingListActivity.kt
@@ -1,20 +1,14 @@
 package com.thinlineit.favorit_android.android.ui.fundingList
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Surface
@@ -27,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -37,6 +32,8 @@ import com.skydoves.landscapist.glide.GlideImage
 import com.thinlineit.favorit_android.android.R
 import com.thinlineit.favorit_android.android.data.Result
 import com.thinlineit.favorit_android.android.data.entity.FundingInfo
+import com.thinlineit.favorit_android.android.ui.createfunding.CreateFundingActivity
+import com.thinlineit.favorit_android.android.ui.detail.FundingDetailActivity
 import com.thinlineit.favorit_android.android.util.shortToast
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -88,37 +85,49 @@ fun Main(viewModel: FundingListViewModel) {
         modifier = Modifier.fillMaxSize(),
         color = com.thinlineit.favorit_android.android.ui.fundingList.ui.theme.FundingListBackgroundColor
     ) {
-        Column() {
-            Spacer(modifier = Modifier.height(27.dp))
-            Row() {
-                Spacer(modifier = Modifier.width(16.dp))
-                Image(painter = painterResource(id = R.drawable.icon_exit), contentDescription = "")
-            }
+        Column {
+            ExitButton()
             Spacer(modifier = Modifier.height(40.dp))
-            Column() {
-                MyFundingList(fundingList = myFundingList)
-                FriendFundingList(fundingList = friendFundingList)
-            }
+            MyFundingList(fundingList = myFundingList)
+            Spacer(modifier = Modifier.height(20.dp))
+            FriendFundingList(fundingList = friendFundingList)
         }
     }
+}
+
+@Composable
+fun ExitButton() {
+    val activity = (LocalContext.current as? Activity)
+    Column() {
+        Spacer(modifier = Modifier.height(27.dp))
+        Row() {
+            Spacer(modifier = Modifier.width(16.dp))
+            Image(painter = painterResource(id = R.drawable.icon_exit),
+                contentDescription = "",
+                modifier = Modifier.clickable {
+                    activity?.finish()
+                })
+        }
+    }
+
 }
 
 
 @Composable
 fun MyFundingList(fundingList: List<FundingInfo>?) {
     Column() {
-        Box() {
+        Box(modifier = Modifier.rotate(3f)) {
             Image(
                 painter = painterResource(id = R.drawable.icon_my_funding),
                 contentDescription = ""
             )
             Row(modifier = Modifier.align(Alignment.CenterStart)) {
-                Spacer(modifier = Modifier.width(10.dp))
+                Spacer(modifier = Modifier.width(20.dp))
                 Text(
                     text = "내가 만든 펀딩(*/ω＼*)",
                     fontSize = 16.sp,
                     color = Color.White,
-                    modifier = Modifier.rotate(2f)
+                    modifier = Modifier.rotate(3f)
                 )
             }
 
@@ -148,18 +157,22 @@ fun MyFundingList(fundingList: List<FundingInfo>?) {
 @Composable
 fun FriendFundingList(fundingList: List<FundingInfo>?) {
     Column(modifier = Modifier.fillMaxWidth()) {
-        Box(modifier = Modifier.align(Alignment.End)) {
+        Box(
+            modifier = Modifier
+                .align(Alignment.End)
+                .rotate(358f)
+        ) {
             Image(
-                painter = painterResource(id = R.drawable.icon_my_funding),
+                painter = painterResource(id = R.drawable.icon_friend_funding),
                 contentDescription = ""
             )
             Row(modifier = Modifier.align(Alignment.CenterStart)) {
-                Spacer(modifier = Modifier.width(10.dp))
+                Spacer(modifier = Modifier.width(40.dp))
                 Text(
                     text = "친구들의 펀딩 (●'e'●)",
                     fontSize = 16.sp,
                     color = Color.White,
-                    modifier = Modifier.rotate(2f)
+                    modifier = Modifier.rotate(357f)
                 )
             }
 
@@ -181,13 +194,16 @@ fun FriendFundingList(fundingList: List<FundingInfo>?) {
     }
 }
 
-
 @Composable
 fun FundingItem(funding: FundingInfo, modifier: Modifier) {
+    val mContext = LocalContext.current
     Box(
         modifier = modifier
             .width(168.dp)
             .height(242.dp)
+            .clickable {
+                FundingDetailActivity.start(mContext, funding.fundingID)
+            }
     ) {
         Image(
             painter = painterResource(id = R.drawable.icon_funding_item),
@@ -221,12 +237,17 @@ fun FundingItem(funding: FundingInfo, modifier: Modifier) {
                 Text(text = funding.name, modifier = Modifier.align(Alignment.BottomCenter))
             }
             Text(text = funding.dueDate, textAlign = TextAlign.Center)
-
         }
     }
 }
 
 @Composable
 fun CreateFunding() {
-    Image(painter = painterResource(id = R.drawable.icon_create_funding), contentDescription = "")
+    val mContext = LocalContext.current
+    Image(
+        painter = painterResource(id = R.drawable.icon_create_funding),
+        contentDescription = "",
+        modifier = Modifier.clickable {
+            CreateFundingActivity.start(mContext)
+        })
 }
